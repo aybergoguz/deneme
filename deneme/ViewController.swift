a1ef4bc387804b7b8e3d6be2fa6638d8cfa5567e
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-22 18:10:14 +0300

    IOS Programlama ilk kod

--- deneme/ViewController.swift
+++ deneme/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var benimLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonTiklandi(_ sender: Any) {
+        
+        benimLabel.text="Beyoğlu Gar Mizah"
+    }
 }
 
